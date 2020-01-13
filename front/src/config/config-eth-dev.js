38d96d8d88aa9dcfd7c54117ced49b9b04abb638
Ilya Teterin <buggzy2@mail.ru>
2020-01-13 21:39:50 +0500

    bet loading

--- front/src/config/config-eth-dev.js
+++ front/src/config/config-eth-dev.js
@@ -1,3 +1,4 @@
 module.exports = {
   escrowAddress: '0x78044fB5ADAA596555288B3F0F939b54C19AeFF2',
+  decimalMultiplicator: Math.pow(10, 1),
 }
--- front/src/store/bets.js
+++ front/src/store/bets.js
@@ -1,5 +1,24 @@
 import Vue from 'vue';
 import _ from 'lodash';
+import config from '../config/config';
+
+const extendBets = (bets, events) => {
+    return(_.map(bets, bet => {
+        return {
+            ...bet,
+            isOpen: (bet.amount - bet.matched) >= 1,
+            isPlaced: (bet.amount - bet.matched) < 1,
+            side: bet.side ? 'for' : 'against',
+            odds: bet.odds / 10,
+            asset: 'USD',
+            eventName: 'TODO event name',
+            subevent: 'todo',
+            cancellable: bet.amount > bet.matched,
+            spentNominal: bet.matched / config.decimalMultiplicator,
+            amountNominal: bet.amount / config.decimalMultiplicator,
+        }
+    }));
+};
 
 export default {
     state: {
@@ -9,6 +28,6 @@ export default {
         onBets: (state, val) => _.forEach(val, bet => Vue.set(state.bets, bet.hash, bet)),
     },
     getters: {
-        getMyBetsExtended: state => state.bets,
+        getMyBetsExtended: (state, getters) => extendBets(state.bets, getters.getEvents),
     },
 };
